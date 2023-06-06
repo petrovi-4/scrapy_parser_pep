ddadb455a48609c86e26939fad006a3621ab836a
petrovi4 <40.10.444@gmail.com>
2023-06-06 16:28:12 +0300

    Version for review 1.1

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -4,23 +4,23 @@ from pep_parse.items import PepParseItem
 
 
 class PepSpider(scrapy.Spider):
-    name = 'pep'
-    allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    name = "pep"
+    allowed_domains = ["peps.python.org"]
+    start_urls = ["https://peps.python.org/"]
 
     def parse(self, response):
         pep_hrefs = response.css('a[href^="pep-"]')
         for link in pep_hrefs:
-            url = response.urljoin(link.attrib['href']) + '/'
+            url = response.urljoin(link.attrib["href"]) + "/"
             yield response.follow(url, callback=self.parse_pep)
 
     def parse_pep(self, response):
         number, name = (
-            response.css('h1.page-title::text').get().split(' – ', 1)
+            response.css("h1.page-title::text").get().split(" – ", 1)
         )
         pep_data = {
-            'number': number.split()[1],
-            'name': name,
-            'status': response.css('abbr::text').get(),
+            "number": number.split()[1],
+            "name": name,
+            "status": response.css("abbr::text").get(),
         }
         yield PepParseItem(pep_data)
